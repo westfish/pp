89a65d34ff24cfc0ba5486024c16f4c58f5537e9
yujun <573009727@qq.com>
2023-03-10 10:27:42 +0800

    rm

--- tests/pipelines/semantic_stable_diffusion/test_semantic_diffusion.py
+++ tests/pipelines/semantic_stable_diffusion/test_semantic_diffusion.py
@@ -127,6 +127,22 @@ class SafeDiffusionPipelineFastTests(unittest.TestCase):
         assert np.abs(image_from_tuple_slice.flatten() - expected_slice).max(
             ) < 0.01
 
+    def test_semantic_diffusion_no_safety_checker(self):
+        pipe = StableDiffusionPipeline.from_pretrained(
+            'hf-internal-testing/tiny-stable-diffusion-lms-pipe',
+            safety_checker=None)
+        assert isinstance(pipe, StableDiffusionPipeline)
+        assert isinstance(pipe.scheduler, LMSDiscreteScheduler)
+        assert pipe.safety_checker is None
+        image = pipe('example prompt', num_inference_steps=2).images[0]
+        assert image is not None
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            pipe.save_pretrained(tmpdirname)
+            pipe = StableDiffusionPipeline.from_pretrained(tmpdirname, from_diffusers=False)
+        assert pipe.safety_checker is None
+        image = pipe('example prompt', num_inference_steps=2).images[0]
+        assert image is not None
+
     def test_semantic_diffusion_pndm(self):
         unet = self.dummy_cond_unet
         scheduler = PNDMScheduler(skip_prk_steps=True)
